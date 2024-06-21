c1fd75747250fdc28c434f3584d5d1428d928d68
typingmonk <tmonk.hsieh@tmonk.tw>
2024-06-21 17:26:28 +0800

    Add collapsable card example for dataLY

---
+++ collapsable_card.blade.php
@@ -0,0 +1,11 @@
+<div class="card shadow mb-4">
+    <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse"
+        role="button" aria-expanded="true" aria-controls="collapseCardExample">
+        <h6 class="m-0 font-weight-bold text-primary">card title</h6>
+    </a>
+    <div class="collapse show" id="collapseCardExample">
+        <div class="card-body">
+            //content
+        </div>
+    </div>
+</div>
